a3f5d98e7967949ff2f6964ec3c330f0d5a22168
AmanGupta1998 <amangupta99a@gmail.com>
2020-09-11 17:59:33 +0530

    Socket Programming

---
+++ 11-sep-2020/SocketClientServer/SocketClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace SocketClient
+{
+    class Program
+    {
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.33"), 1111);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch
+            {
+                Console.WriteLine("unable to remote end point");
+            }
+            Console.WriteLine("enter the text: ");
+            String text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            sck.Send(data);
+            Console.WriteLine("Data sent ");
+            Console.WriteLine("Press any to continue");
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
---
+++ 11-sep-2020/SocketClientServer/SocketClientServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.SqlServer.Server;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static byte[] Buffer { get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.33"), 1111));
+            sck.Listen(2);
+            Socket Acceptedd = sck.Accept();
+            Buffer = new byte[Acceptedd.SendBufferSize];
+            int byteRead = Acceptedd.Receive(Buffer);
+            byte[] formatted = new byte[byteRead];
+            int i;
+            for ( i = 0; i < byteRead;i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strdata = Encoding.ASCII.GetString(formatted);
+            Console.WriteLine(strdata);
+            Console.Read();
+            sck.Close();
+            Acceptedd.Close();
+        }
+    }
+}
